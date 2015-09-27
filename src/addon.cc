59b6396cc154b9711159b34c3e49e4fb1790ba25
Isaac Whitfield <iwhitfield@appcelerator.com>
2015-09-27 02:53:47 +0100

    Update to latest version of nan, fix #1

--- src/addon.cc
+++ src/addon.cc
@@ -3,8 +3,8 @@
 using namespace v8;
 
 void Init(Handle<Object> exports) {
-	exports->Set(NanNew<String>("Integer"), NanNew<FunctionTemplate>(HashSet<int64_t>::Init)->GetFunction());
-	exports->Set(NanNew<String>("String"), NanNew<FunctionTemplate>(HashSet<std::string>::Init)->GetFunction());
+	exports->Set(Nan::New<String>("Integer").ToLocalChecked(), Nan::New<FunctionTemplate>(HashSet<int64_t>::Init)->GetFunction());
+	exports->Set(Nan::New<String>("String").ToLocalChecked(), Nan::New<FunctionTemplate>(HashSet<std::string>::Init)->GetFunction());
 }
 
 NODE_MODULE(addon, Init);
--- src/hashset.h
+++ src/hashset.h
@@ -14,241 +14,241 @@ template <typename T> T UnwrapValue(const Local<Value> &arg);
 template <typename T> Handle<Value> WrapValue(const T &value);
 
 template <typename K>
-class HashSet : public node::ObjectWrap {
+class HashSet : public Nan::ObjectWrap {
 private:
-	std::unordered_set<K> set;
+    std::unordered_set<K> set;
 
-	class Iterator : public node::ObjectWrap {
-	private:
-		const std::unordered_set<K> *set;
-		typename std::unordered_set<K>::const_iterator iterator;
+    class Iterator : public Nan::ObjectWrap {
+    private:
+        const std::unordered_set<K> *set;
+        typename std::unordered_set<K>::const_iterator iterator;
 
-	public:
-		Iterator(const std::unordered_set<K> *set) : set(set) {
-			iterator = set->begin();
-		}
+    public:
+        Iterator(const std::unordered_set<K> *set) : set(set) {
+            iterator = set->begin();
+        }
 
-		static Handle<Value> New(const std::unordered_set<K> *set) {
-			Local<Object> obj = NanNew<FunctionTemplate>(GetCtor)->GetFunction()->NewInstance();
-			Iterator *it = new Iterator(set);
+        static Handle<Value> New(const std::unordered_set<K> *set) {
+            Local<Object> obj = Nan::New<FunctionTemplate>(GetCtor)->GetFunction()->NewInstance();
+            Iterator *it = new Iterator(set);
 
-			it->Wrap(obj);
+            it->Wrap(obj);
 
-			return obj;
-		}
+            return obj;
+        }
 
-		static NAN_METHOD(GetCtor){
-			NanScope();
+        static NAN_METHOD(GetCtor){
+            Nan::HandleScope scope;
 
-			Local<FunctionTemplate> tpl = NanNew<FunctionTemplate>();
-			tpl->SetClassName(NanNew<String>("HashSetIt"));
-			tpl->InstanceTemplate()->SetInternalFieldCount(1);
+            Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>();
+            tpl->SetClassName(Nan::New<String>("HashSetIt").ToLocalChecked());
+            tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-			auto prototype = tpl->PrototypeTemplate();
-			prototype->Set(NanNew<String>("hasNext"), NanNew<FunctionTemplate>(HasNext));
-			prototype->Set(NanNew<String>("next"), NanNew<FunctionTemplate>(Next));
+            auto prototype = tpl->PrototypeTemplate();
+            prototype->Set(Nan::New<String>("hasNext").ToLocalChecked(), Nan::New<FunctionTemplate>(HasNext));
+            prototype->Set(Nan::New<String>("next").ToLocalChecked(), Nan::New<FunctionTemplate>(Next));
 
-			NanReturnValue(NanNew(tpl->GetFunction())->NewInstance());
-		}
+            info.GetReturnValue().Set(Nan::NewInstance(tpl->GetFunction()).ToLocalChecked());
+        }
 
-		static NAN_METHOD(HasNext) {
-			NanScope();
+        static NAN_METHOD(HasNext) {
+            Nan::HandleScope scope;
 
-			Iterator *obj = node::ObjectWrap::Unwrap<Iterator>(args.Holder());
+            Iterator *obj = Nan::ObjectWrap::Unwrap<Iterator>(info.Holder());
 
-			NanReturnValue(NanNew<Boolean>(obj->iterator != obj->set->end()));
-		}
+            info.GetReturnValue().Set(Nan::New<Boolean>(obj->iterator != obj->set->end()));
+        }
 
-		static NAN_METHOD(Next) {
-			NanScope();
+        static NAN_METHOD(Next) {
+            Nan::HandleScope scope;
 
-			Iterator *obj = node::ObjectWrap::Unwrap<Iterator>(args.Holder());
+            Iterator *obj = Nan::ObjectWrap::Unwrap<Iterator>(info.Holder());
 
-			K value = *obj->iterator;
-			obj->iterator++;
+            K value = *obj->iterator;
+            obj->iterator++;
 
-			NanReturnValue(WrapValue(value));
-		}
-	};
+            info.GetReturnValue().Set(WrapValue(value));
+        }
+    };
 
-	static NAN_METHOD(New) {
-		NanScope();
+    static NAN_METHOD(New) {
+        Nan::HandleScope scope;
 
-		HashSet *obj = new HashSet();
+        HashSet *obj = new HashSet();
 
-		obj->Wrap(args.Holder());
+        obj->Wrap(info.Holder());
 
-		NanReturnValue(args.Holder());
-	}
+        info.GetReturnValue().Set(info.Holder());
+    }
 
-	static NAN_METHOD(Add) {
-		NanScope();
+    static NAN_METHOD(Add) {
+        Nan::HandleScope scope;
 
-		HashSet *obj = node::ObjectWrap::Unwrap<HashSet>(args.Holder());
+        HashSet *obj = Nan::ObjectWrap::Unwrap<HashSet>(info.Holder());
 
-		if (args.Length() < 1) {
-			return NanThrowError("No key provided!");
-		} else if(!IsA<K>(args[0])) {
-			return NanThrowTypeError("Invalid key type provided!");
-		}
+        if (info.Length() < 1) {
+            return Nan::ThrowError("No key provided!");
+        } else if(!IsA<K>(info[0])) {
+            return Nan::ThrowTypeError("Invalid key type provided!");
+        }
 
-		K key = UnwrapValue<K>(args[0]);
+        K key = UnwrapValue<K>(info[0]);
 
-		obj->set.insert(key);
+        obj->set.insert(key);
 
-		NanReturnUndefined();
-	}
+        return;
+    }
 
-	static NAN_METHOD(Buckets) {
-		NanScope();
+    static NAN_METHOD(Buckets) {
+        Nan::HandleScope scope;
 
-		HashSet *obj = node::ObjectWrap::Unwrap<HashSet>(args.Holder());
-		Local<Array> buckets = NanNew<Array>();
+        HashSet *obj = Nan::ObjectWrap::Unwrap<HashSet>(info.Holder());
+        Local<Array> buckets = Nan::New<Array>();
 
-		for (unsigned i = 0; i < obj->set.bucket_count(); i++) {
-			Local<Array> bucket = NanNew<Array>();
-			unsigned x = 0;
-			for (auto it = obj->set.begin(i); it != obj->set.end(i); it++) {
-				bucket->Set(x++, WrapValue(*it));
-			}
-			buckets->Set(i, bucket);
-		}
+        for (unsigned i = 0; i < obj->set.bucket_count(); i++) {
+            Local<Array> bucket = Nan::New<Array>();
+            unsigned x = 0;
+            for (auto it = obj->set.begin(i); it != obj->set.end(i); it++) {
+                bucket->Set(x++, WrapValue(*it));
+            }
+            buckets->Set(i, bucket);
+        }
 
-		NanReturnValue(NanNew(buckets));
-	}
+        info.GetReturnValue().Set(buckets);
+    }
 
-	static NAN_METHOD(Clear) {
-		NanScope();
+    static NAN_METHOD(Clear) {
+        Nan::HandleScope scope;
 
-		HashSet *obj = node::ObjectWrap::Unwrap<HashSet>(args.Holder());
+        HashSet *obj = Nan::ObjectWrap::Unwrap<HashSet>(info.Holder());
 
-		obj->set.clear();
+        obj->set.clear();
 
-		NanReturnUndefined();
-	}
+        return;
+    }
 
-	static NAN_METHOD(Contains) {
-		NanScope();
+    static NAN_METHOD(Contains) {
+        Nan::HandleScope scope;
 
-		HashSet *obj = node::ObjectWrap::Unwrap<HashSet>(args.Holder());
+        HashSet *obj = Nan::ObjectWrap::Unwrap<HashSet>(info.Holder());
 
-		if (args.Length() < 1) {
-			return NanThrowError("No key provided!");
-		} else if(!IsA<K>(args[0])) {
-			return NanThrowTypeError("Invalid key type provided!");
-		}
+        if (info.Length() < 1) {
+            return Nan::ThrowError("No key provided!");
+        } else if(!IsA<K>(info[0])) {
+            return Nan::ThrowTypeError("Invalid key type provided!");
+        }
 
-		K key = UnwrapValue<K>(args[0]);
+        K key = UnwrapValue<K>(info[0]);
 
-		NanReturnValue(NanNew<Number>(obj->set.count(key) > 0));
-	}
+        info.GetReturnValue().Set(Nan::New<Number>(obj->set.count(key) > 0));
+    }
 
-	static NAN_METHOD(Count) {
-		NanScope();
+    static NAN_METHOD(Count) {
+        Nan::HandleScope scope;
 
-		HashSet *obj = node::ObjectWrap::Unwrap<HashSet>(args.Holder());
+        HashSet *obj = Nan::ObjectWrap::Unwrap<HashSet>(info.Holder());
 
-		if (args.Length() < 1) {
-			return NanThrowError("No key provided!");
-		} else if(!IsA<K>(args[0])) {
-			return NanThrowTypeError("Invalid key type provided!");
-		}
+        if (info.Length() < 1) {
+            return Nan::ThrowError("No key provided!");
+        } else if(!IsA<K>(info[0])) {
+            return Nan::ThrowTypeError("Invalid key type provided!");
+        }
 
-		K key = UnwrapValue<K>(args[0]);
+        K key = UnwrapValue<K>(info[0]);
 
-		NanReturnValue(NanNew<Number>(obj->set.count(key)));
-	}
+        info.GetReturnValue().Set(Nan::New<Number>(obj->set.count(key)));
+    }
 
-	static NAN_METHOD(Empty){
-		NanScope();
+    static NAN_METHOD(Empty){
+        Nan::HandleScope scope;
 
-		HashSet *obj = node::ObjectWrap::Unwrap<HashSet>(args.Holder());
+        HashSet *obj = Nan::ObjectWrap::Unwrap<HashSet>(info.Holder());
 
-		NanReturnValue(NanNew<Boolean>(obj->set.empty()));
-	}
+        info.GetReturnValue().Set(obj->set.empty());
+    }
 
-	static NAN_METHOD(Iterator){
-		NanScope();
+    static NAN_METHOD(Iterator){
+        Nan::HandleScope scope;
 
-		HashSet *obj = node::ObjectWrap::Unwrap<HashSet>(args.Holder());
+        HashSet *obj = Nan::ObjectWrap::Unwrap<HashSet>(info.Holder());
 
-		NanReturnValue(NanNew(Iterator::New(&obj->set)));
-	}
+        info.GetReturnValue().Set(Iterator::New(&obj->set));
+    }
 
-	static NAN_METHOD(Keys){
-		NanScope();
+    static NAN_METHOD(Keys){
+        Nan::HandleScope scope;
 
-		HashSet *obj = node::ObjectWrap::Unwrap<HashSet>(args.Holder());
-		Local<Array> array = NanNew<Array>();
+        HashSet *obj = Nan::ObjectWrap::Unwrap<HashSet>(info.Holder());
+        Local<Array> array = Nan::New<Array>();
 
-		unsigned i = 0;
-		for (const auto& elem: obj->set) {
-			array->Set(i++, WrapValue(elem));
-		}
+        unsigned i = 0;
+        for (const auto& elem: obj->set) {
+            array->Set(i++, WrapValue(elem));
+        }
 
-		NanReturnValue(NanNew(array));
-	}
+        info.GetReturnValue().Set(array);
+    }
 
-	static NAN_METHOD(Remove) {
-		NanScope();
+    static NAN_METHOD(Remove) {
+        Nan::HandleScope scope;
 
-		HashSet *obj = node::ObjectWrap::Unwrap<HashSet>(args.Holder());
+        HashSet *obj = Nan::ObjectWrap::Unwrap<HashSet>(info.Holder());
 
-		if (args.Length() < 1) {
-			return NanThrowError("No key provided!");
-		} else if (!IsA<K>(args[0])) {
-			return NanThrowTypeError("Invalid key type provided!");
-		}
+        if (info.Length() < 1) {
+            return Nan::ThrowError("No key provided!");
+        } else if (!IsA<K>(info[0])) {
+            return Nan::ThrowTypeError("Invalid key type provided!");
+        }
 
-		K key = UnwrapValue<K>(args[0]);
+        K key = UnwrapValue<K>(info[0]);
 
-		obj->set.erase(key);
+        obj->set.erase(key);
 
-		NanReturnUndefined();
-	}
+        return;
+    }
 
-	static NAN_METHOD(Size){
-		NanScope();
+    static NAN_METHOD(Size){
+        Nan::HandleScope scope;
 
-		HashSet *obj = node::ObjectWrap::Unwrap<HashSet>(args.Holder());
+        HashSet *obj = Nan::ObjectWrap::Unwrap<HashSet>(info.Holder());
 
-		NanReturnValue(NanNew<Number>(obj->set.size()));
-	}
+        info.GetReturnValue().Set(Nan::New<Number>(obj->set.size()));
+    }
 
     static NAN_METHOD(Type){
-        NanScope();
-        NanReturnValue(NanNew<String>(GetType<K>()));
+        Nan::HandleScope scope;
+        info.GetReturnValue().Set(Nan::New<String>(GetType<K>()).ToLocalChecked());
     }
 
 public:
-	static NAN_METHOD(Init){
-		NanScope();
+    static NAN_METHOD(Init){
+        Nan::HandleScope scope;
 
-		Local<FunctionTemplate> tpl = NanNew<FunctionTemplate>(New);
-		tpl->SetClassName(NanNew<String>("HashSet"));
-		tpl->InstanceTemplate()->SetInternalFieldCount(1);
+        Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>(New);
+        tpl->SetClassName(Nan::New<String>("HashSet").ToLocalChecked());
+        tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-		auto prototype = tpl->PrototypeTemplate();
-		prototype->Set(NanNew<String>("add"), NanNew<FunctionTemplate>(Add));
-		prototype->Set(NanNew<String>("buckets"), NanNew<FunctionTemplate>(Buckets));
-		prototype->Set(NanNew<String>("clear"), NanNew<FunctionTemplate>(Clear));
-		prototype->Set(NanNew<String>("contains"), NanNew<FunctionTemplate>(Contains));
-		prototype->Set(NanNew<String>("count"), NanNew<FunctionTemplate>(Count));
-		prototype->Set(NanNew<String>("empty"), NanNew<FunctionTemplate>(Empty));
-		prototype->Set(NanNew<String>("iterator"), NanNew<FunctionTemplate>(Iterator));
-		prototype->Set(NanNew<String>("keys"), NanNew<FunctionTemplate>(Keys));
-		prototype->Set(NanNew<String>("remove"), NanNew<FunctionTemplate>(Remove));
-		prototype->Set(NanNew<String>("size"), NanNew<FunctionTemplate>(Size));
-        prototype->Set(NanNew<String>("type"), NanNew<FunctionTemplate>(Type));
+        auto prototype = tpl->PrototypeTemplate();
+        prototype->Set(Nan::New<String>("add").ToLocalChecked(), Nan::New<FunctionTemplate>(Add));
+        prototype->Set(Nan::New<String>("buckets").ToLocalChecked(), Nan::New<FunctionTemplate>(Buckets));
+        prototype->Set(Nan::New<String>("clear").ToLocalChecked(), Nan::New<FunctionTemplate>(Clear));
+        prototype->Set(Nan::New<String>("contains").ToLocalChecked(), Nan::New<FunctionTemplate>(Contains));
+        prototype->Set(Nan::New<String>("count").ToLocalChecked(), Nan::New<FunctionTemplate>(Count));
+        prototype->Set(Nan::New<String>("empty").ToLocalChecked(), Nan::New<FunctionTemplate>(Empty));
+        prototype->Set(Nan::New<String>("iterator").ToLocalChecked(), Nan::New<FunctionTemplate>(Iterator));
+        prototype->Set(Nan::New<String>("keys").ToLocalChecked(), Nan::New<FunctionTemplate>(Keys));
+        prototype->Set(Nan::New<String>("remove").ToLocalChecked(), Nan::New<FunctionTemplate>(Remove));
+        prototype->Set(Nan::New<String>("size").ToLocalChecked(), Nan::New<FunctionTemplate>(Size));
+        prototype->Set(Nan::New<String>("type").ToLocalChecked(), Nan::New<FunctionTemplate>(Type));
 
-		NanReturnValue(NanNew(tpl->GetFunction())->NewInstance());
-	}
+        info.GetReturnValue().Set(Nan::NewInstance(tpl->GetFunction()).ToLocalChecked());
+    }
 };
 
 // int64_t specialization
 template <>
 bool IsA<int64_t>(const Local<Value> &arg) {
-	return arg->IsNumber();
+    return arg->IsNumber();
 }
 
 template <>
@@ -258,18 +258,18 @@ std::string GetType<int64_t>() {
 
 template <>
 int64_t UnwrapValue<int64_t>(const Local<Value> &arg) {
-	return arg->IntegerValue();
+    return arg->IntegerValue();
 }
 
 template <>
 Handle<Value> WrapValue<int64_t>(const int64_t &value) {
-	return NanNew<Number>(value);
+    return Nan::New<Number>(value);
 }
 
 // std::string specialization
 template <>
 bool IsA<std::string>(const Local<Value> &arg) {
-	return arg->IsString();
+    return arg->IsString();
 }
 
 template <>
@@ -279,14 +279,14 @@ std::string GetType<std::string>() {
 
 template <>
 std::string UnwrapValue<std::string>(const Local<Value> &arg) {
-	Local<String> local = Local<String>::Cast(arg);
-	String::Utf8Value utf(local);
-	return *utf;
+    Local<String> local = Local<String>::Cast(arg);
+    String::Utf8Value utf(local);
+    return *utf;
 }
 
 template <>
 Handle<Value> WrapValue<std::string>(const std::string &value) {
-	return NanNew<String>(value.c_str());
+    return Nan::New<String>(value.c_str()).ToLocalChecked();
 }
 
 #endif
